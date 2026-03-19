34769e1e562f56e4a5a735322f7ae97d57449228
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-19 07:45:21 -0600

    Clase ejercicio1, listo

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+
+
+
+
+public class Estudiante()
+{
+    public string nombre;
+    public int nota1;
+    public int nota2;
+    public int nota3;
+
+    public double CalcularPromedio()
+    {
+        return (nota1+nota2+nota3)/3;
+    }
+    public string Estado()
+    {
+        if (CalcularPromedio() >= 61)
+        {
+            return "Aprobado";
+        }
+        else
+        {
+            return "Repobado";
+        }
+    }
+     public void MostraarDatos()
+    { Console.WriteLine($"Nombre:_ {nombre} | Promedio:_ {CalcularPromedio()} | Estado del estudiante:_ {Estado()} ");
+    }
+
+}
